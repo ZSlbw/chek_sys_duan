46052579b7163d0f9c52d2dd1a4cdc5a93029af3
ZSlbw <814088900@qq.com>
2021-03-04 18:42:52 +0800

    求出内存数据大小端的存储方式

---
+++ 求出本机大端小端1.c
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int chek_sys()
+{
+	int a = 1;
+	char* p = (char*)&a;
+	return *p;
+}
+int main()
+{
+	int ret = chek_sys();
+	if (ret == 1)
+	{
+		printf("Ð¡¶Ë\n");
+	}
+	else
+	{
+		printf("´ó¶Ë\n");
+	}
+	return 0;
+}
